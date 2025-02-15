17968d5a0ae6b55855d46a33158d9c5258375a00
vladimirrubtsov <Vladimir08!>
2025-02-15 08:47:15 +0300

    lab 5 is done

--- app/views.py
+++ app/views.py
@@ -3,12 +3,15 @@ from lib2to3.fixes.fix_input import context
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import render
 from django.template.context_processors import request
-
+from django.views.generic import TemplateView
+from django.views import View
 from .models import get_random_text
 from django.http import HttpRequest, JsonResponse
 from django.shortcuts import redirect
 from django.contrib.auth import login, logout, authenticate
 from .forms import TemplateForm, CustomUserCreationForm
+from django.views.generic import FormView
+from django.contrib.auth.views import LoginView
 
 
 def template_view(request):
@@ -97,3 +100,65 @@ def get_text_json(request):
         return JsonResponse({"text": get_random_text()},
                             json_dumps_params={"ensure_ascii": False})
 
+
+class TemplView(View):
+    def get(self, request):
+        return render(request, 'app/template_form.html')
+    # TODO скопируйте код, что есть в template_view в теле условия request.method == "GET"
+
+    def post(self, request):
+
+        form = TemplateForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            my_text = form.cleaned_data.get("my_text")
+            my_email = form.cleaned_data.get("my_email")
+            my_select = form.cleaned_data.get("my_select")
+            my_textarea = form.cleaned_data.get("my_textarea")
+            my_pass = form.cleaned_data.get("my_pass")
+            my_date = form.cleaned_data.get("my_date")
+            my_int = form.cleaned_data.get("my_int")
+            my_check = form.cleaned_data.get("my_check")
+            return JsonResponse(data=[my_text, my_check, my_date, my_email, my_int, my_pass, my_select, my_textarea],
+                                safe=False, json_dumps_params={"indent": 4})
+        # TODO Проведите здесь получение и обработку данных если это необходимо
+        return render(request, 'app/template_form.html', context={"form": form})
+# TODO скопируйте код, что есть в template_view в теле условия request.method == "POST"
+
+
+class MyTemplView(TemplateView):
+    template_name = 'app/template_form.html'
+
+    def post(self, request, *args, **kwargs):
+        form = TemplateForm(request.POST)
+        if form.is_valid():
+            # print(form.cleaned_data)
+            my_text = form.cleaned_data.get("my_text")
+            my_email = form.cleaned_data.get("my_email")
+            my_select = form.cleaned_data.get("my_select")
+            my_textarea = form.cleaned_data.get("my_textarea")
+            my_pass = form.cleaned_data.get("my_pass")
+            my_date = form.cleaned_data.get("my_date")
+            my_int = form.cleaned_data.get("my_int")
+            my_check = form.cleaned_data.get("my_check")
+            return JsonResponse(data=[my_text, my_check, my_date, my_email, my_int, my_pass, my_select, my_textarea],
+                                safe=False, json_dumps_params={"indent": 4})
+        # TODO Проведите здесь получение и обработку данных если это необходимо
+        context = self.get_context_data(**kwargs)  # Получаем контекст, если он есть
+        context["form"] = form  # Записываем в контекст форму
+        return self.render_to_response(context)  # Возвращаем вызов метода render_to_response
+        # TODO прописываем всё что было в методе post в TemplView(View)
+
+class MyFormView(FormView):
+    template_name = 'app/template_form.html'
+    form_class = TemplateForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        return JsonResponse(form.cleaned_data)
+
+class MyLoginView(LoginView):
+    template_name = 'app/login.html'
+    redirect_authenticated_user = True  # Данный флаг не позволит авторизированному
+    # пользователю зайти на страницу с авторизацией и сразу его перенаправит на
+    # ссылку редиректа. По умолчанию redirect_authenticated_user = False
--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,15 @@ AUTHENTICATION_BACKENDS = (
    'django.contrib.auth.backends.ModelBackend',
 )
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' # Пример настроек для использования консольного бэкенда (для тестирования)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.ethereal.email'
+EMAIL_HOST_USER = 'bernhard.konopelski85@ethereal.email'
+EMAIL_HOST_PASSWORD = 'nS4nkVKxrwPFDFg8jM'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+
+
 LOGIN_REDIRECT_URL = '/profile/' # Чтобы после авторизации перенаправлялось на страницу пользователя
 
 SOCIAL_AUTH_GITHUB_KEY = 'Ov23liyjpmZ6nZvo6ocK'
